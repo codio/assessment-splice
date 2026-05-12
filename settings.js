4f4978051e8bdf9b59e69a7898a5bdb75a97547d
Mikhail Loginov <mloginov@codio.com>
2026-05-12 14:45:54 +0100

    setting and assessment rendering

---
+++ settings.js
@@ -0,0 +1,39 @@
+(function () {
+  const collectSettings = () => {
+    const instructions = $('#instructions').val()
+    const url = $('#url').val()
+    return {instructions, url};
+  }
+
+  const exportSettings = () => {
+    const data = collectSettings();
+    window.codioAssessmentsHelper.send(window.codioAssessmentsHelper.METHODS.EXPORT_SETTINGS_RESPONSE, data);
+  }
+
+  const applySettings = (settings = {}) => {
+    $('#instructions').val(settings.instructions || '');
+    $('#url').val(settings.url || '');
+  }
+
+  const processMessage = (jsonData) => {
+    console.log('settings iframe processMessage', jsonData)
+    try {
+      const {method, data} = JSON.parse(jsonData);
+      switch (method) {
+        case window.codioAssessmentsHelper.METHODS.EXPORT_SETTINGS:
+          exportSettings();
+          break;
+        case window.codioAssessmentsHelper.METHODS.GET_SETTINGS_RESPONSE:
+          applySettings(data.settings);
+          break;
+      }
+    } catch {}
+  }
+
+  const onLoad = async () => {
+    window.codioAssessmentsHelper.registerMessageListener(processMessage)
+    window.codioAssessmentsHelper.send(window.codioAssessmentsHelper.METHODS.GET_SETTINGS)
+  }
+
+  window.addEventListener('load', onLoad);
+})()
